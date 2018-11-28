26be8a6a03c3d75ad3c932de9d65b95881a2c237
彭定林 <1979945716@qq.com>
2018-11-28 23:36:16 +0800

    完成个人简介组件introduction样式。

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Introduction.css'
+import avatar from '../../images/avatar.jpeg'
 
 class Introduction extends React.Component {
     render() {
         return (
             <div className="introduction">
+                <img 
+                className="introduction_avatar"
+                src={avatar}/>
+                <hr/>
+                <div className="introduction_text">
+                    <p>
+                        Tag:    前端 java
+                    </p>
+                    <p>偶尔向往着AI</p>
+                    <p>结果目前在全干工程师的道路上一路向北……</p>
+                </div>
             </div>
         )
     }
