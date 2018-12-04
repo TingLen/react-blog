ed8320131b2ddfb087a08531f281da2aec08bb46
彭定林 <1979945716@qq.com>
2018-12-05 01:02:15 +0800

    封装axios

---
+++ src/http/http.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import { resolve } from 'upath';
+
+axios.defaults.baseURL = "/"
+axios.defaults.timeout = 10000
+
+
+// Add a request interceptor
+axios.interceptors.request.use( config => {
+    // 加载动画
+    return config
+  }, err => {
+    // Do something with request error
+    return Promise.reject(err)
+  });
+
+// Add a response interceptor
+axios.interceptors.response.use( response =>  {
+    // Do something with response data
+    let data = response.data
+    
+    return data
+  }, err => {
+    // Do something with response error
+    console.log(err)
+    return Promise.reject(err)
+  });
+
+
+
+export const get = (url,params) => {
+    return axios({
+        method: 'get',
+        url,
+        params
+    })
+}
+
+export const post = (url,params) => {
+    return axios({
+        method: 'post',
+        url,
+        params
+    })
+}
